5f48d5990b206e09af321cacb8f5d560d128350a
Abhrajit Gupta <abhrajitgupta.it2022@nsec.ac.in>
2025-02-19 21:59:40 +0530

    added title

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 // export const metadata: Metadata = {
 //   title: "Taskdrawer",
 //   description: "Your own Task Management Platform",
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,15 @@ import Hero from '@/components/Hero'
 import Features from "@/components/Features"
 import CTA from "@/components/CTA"
 import Footer from "@/components/Footer"
+import { useEffect } from "react";
 
 export default function Home() {
 
+
+  useEffect(() => {
+    document.title = "Taskdrawer : Your Personal Task Management Platform"
+  }, [])
+  
 
   return (
     <main className="min-h-screen bg-white">
